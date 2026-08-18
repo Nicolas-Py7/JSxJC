8e8a07108b1bb45c4e224021782a2088324959a3
Nicolas <nicolas.p@escolar.ifrn.edu.br>
2026-08-18 17:37:08 -0300

    finalização do projeto

--- src/components/GraficoJuros.jsx
+++ src/components/GraficoJuros.jsx
@@ -1,4 +1,5 @@
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, plugins } from 'chart.js'
+import { color } from 'chart.js/helpers'
 import { Line } from 'react-chartjs-2'
 
 ChartJS.register(
@@ -12,6 +13,8 @@ function GraficoJuros({dados}) {
             {
                 label: 'Juros Simples',
                 data: dados.valoresSimples,
+                borderColor: '#60A5FA',
+                backgroundColor: '#60A5FA',
                 borderWidth: 3,
                 pointRadius: 3
             },
@@ -19,6 +22,8 @@ function GraficoJuros({dados}) {
             {
                 label: 'Juros Compostos',
                 data: dados.valoresCompostos,
+                borderColor: '#2563EB',
+                backgroundColor: '#2563EB',
                 borderWidth: 3,
                 pointRadius: 3
             }
@@ -31,12 +36,40 @@ function GraficoJuros({dados}) {
 
     plugins: {
         legend: {
-            display: true
+            display: true,
+            labels: {
+                color: '#FFFFFF'
+            }
         },
 
         title: {
             display: true,
-            text: 'Comparação entre Juros Simples e Compostos'
+            text: 'Comparação entre Juros Simples e Compostos',
+            color: '#FFFFFF',
+            font: {
+                size: 18,
+                weigth: 'bold'
+            }
+        }
+    },
+
+    scales: {
+        x: {
+            ticks: {
+                color: '#CBD5E1'
+            },
+            grid: {
+                color: '#334155'
+            }
+        },
+
+        y: {
+            ticks: {
+                color: '#CBD5E1'
+            },
+            grid: {
+                color: '#334155'
+            }
         }
     }
 }
